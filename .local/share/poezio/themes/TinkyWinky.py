e5f6959faa9c9da10a1205acec2f40262f09e875
KronoZed <root@kronoz.guru>
2016-02-13 12:14:50 +0300

    Create TinkyWinky.py

---
+++ .local/share/poezio/themes/TinkyWinky.py
@@ -0,0 +1,9 @@
+import theming
+
+class TinkyWinky(theming.Theme):
+    COLOR_TAB_NORMAL = (7, 5)
+    COLOR_INFORMATION_BAR = (7, 5)
+    COLOR_TAB_NEW_MESSAGE = (7, 4)
+    COLOR_TOPIC_BAR = (7, 5)
+    COLOR_GROUPCHAT_NAME = (7, 5)
+theme = TinkyWinky()
