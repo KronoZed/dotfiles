49f590fbdf9d1186aa8684ee30583824dbd8db57
KronoZed <root@kronoz.guru>
2016-02-22 05:58:26 +0300

    Update screenshot.sh

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,1 +1,1 @@
-scrot '%Y-%m-%d_%H%M%S_$wx$h.png' -e 'mv $f ~/Картинки/screenshots/ && notify-send "Скриншот сделан и перенесён в папку ~/screenshots/"'
+scrot '%Y-%m-%d_%H-%M-%S_$wx$h.png' -e 'mv $f ~/Картинки/screenshots/ && notify-send "Скриншот сделан и перенесён в папку ~/Картинки/screenshots/"'
