7fcd0005a50bbc1adc6c75ec767ce6ded1510473
KronoZed <root@kronoz.guru>
2016-02-22 22:25:38 +0300

    Update upload-full-shot.sh

--- bin/upload-full-shot.sh
+++ bin/upload-full-shot.sh
@@ -6,9 +6,15 @@ else
     FILENAME="$(date +'%Y-%m-%d_at_%H-%M-%S')_$HOSTNAME.png"
 fi
 
-scrot -q 100 ~/share/root.kronoz.guru/screenshots/${FILENAME}
-URI="http://root.kronoz.guru/screenshots/${FILENAME}"
-echo -n $URI | tee >(xclip) && echo
-notify-send "Скриншот сделан и загружен на сервер root.kronoz.guru"
-chromium http://root.kronoz.guru/screenshots/${FILENAME}
+if  scrot -q 100 ~/share/root.kronoz.guru/screenshots/${FILENAME} ; then
+    notify-send -u low -t 1500 "Скриншот ${FILENAME} сделан и загружен на сервер root.kronoz.guru"
+
+    URI="http://root.kronoz.guru/screenshots/${FILENAME}"
+    echo -n $URI | tee >(xclip) && echo
+
+    chromium http://root.kronoz.guru/screenshots/${FILENAME}
+else
+    notify-send -u critical -t 3000 "Не удалось сделать скриншот"
+fi
+
 exit
